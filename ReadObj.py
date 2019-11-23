5a02e13519ce3b4e1576b4214af44eab3ac30064
Sw24sX <aleksandr.korolyov.99@users.noreply.github.com>
2019-11-24 01:39:35 +0500

    End create readObj

--- ReadObj.py
+++ ReadObj.py
@@ -1,55 +1,30 @@
-import math
-
-
 def read_file(path: str):
-    # TODO
-    #  Работает не правильно, переделать
     """
-    Возвращает список мешей из файла по указаному пути
+    парсит файл на меши
     :param path: путь к файлу
-    :return: список в формате [[mesh], [mesh], ...], где [mesh] это [[v1], [v2], [v3]], где [v] это [x, y, z],
-        [min_x, max_x], [min_y, max_y], [min_z, max_z]
+    :return:
     """
-    res = []
+    vertexes = _get_vertexes(path)
+    result = []
     with open(path, "r") as file:
-        points = _get_list_vertex(file)
-        for string in file:
-            temp = string.split()
-            if len(temp) > 0 and temp[0] == "f":
-                temp = [i.split("/")[0] for i in temp[1:]]
-                res.append([points[0][int(i) - 1] for i in temp])
-
-    return res, points[1], points[2], points[3]
+        for line in file:
+            line = line.strip().split()
+            if len(line) > 0 and line[0].lower() == "f":
+                line = [int(i.split("/")[0]) for i in line[1:]]
+                result.append([vertexes[i - 1] for i in line])
+    return result
 
 
-def _get_list_vertex(file):
-    points = []
+def _get_vertexes(path: str):
+    result = []
+    with open(path) as file:
+        for line in file:
+            line = line.strip().split()
+            if len(line) > 0 and line[0].lower() == "v":
+                result.append([float(i) for i in line[1:]])
+    return result
 
-    min_x = math.inf
-    max_x = 0
-    min_y = math.inf
-    max_y = 0
-    min_z = math.inf
-    max_z = 0
 
-    for string in file:
-        temp = string.split()
-        if len(temp) > 0 and temp[0].lower() == "v":
-            points.append([float(i) for i in temp[1:]])
-            if min_x > float(temp[1]):
-                min_x = float(temp[1])
-            if min_y > float(temp[2]):
-                min_y = float(temp[2])
-            if min_z > float(temp[3]):
-                min_z = float(temp[3])
-
-            if max_x < float(temp[1]):
-                max_x = float(temp[1])
-            if max_y < float(temp[2]):
-                max_y = float(temp[2])
-            if max_z < float(temp[3]):
-                max_z = float(temp[3])
-
-        if len(points) > 0 and (len(temp) > 0 or temp[0].lower() == "f"):
-            break
-    return points, [min_x, max_x], [min_y, max_y], [min_z, max_z]
+def _check_line(line: str, letter: str):
+    line = line.strip().split()
+    return len(line) > 0 and line[0].lower() == letter

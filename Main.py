0949906fac5b0d4926507178a86a3a1d0bc0243e
Sw24sX <aleksandr.korolyov.99@users.noreply.github.com>
2019-11-24 18:48:02 +0500

    Save

--- Main.py
+++ Main.py
@@ -3,8 +3,8 @@ import ReadObj
 import Visualization
 
 
-path = "experiments/deer.obj"
-size_voxel = 1.0
+path = "experiments/sphere.obj"
+size_voxel = 0.1
 model = ReadObj.read_file(path)
 voxels = MeshToVoxel.get_voxel_model(model[0], model[1], size_voxel)
 Visualization.get_model(voxels, size_voxel)

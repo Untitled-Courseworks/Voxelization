e8733979aa81c7c6713cfb3b48caf697fa507aee
Sw24sX <aleksandr.korolyov.99@users.noreply.github.com>
2019-11-28 01:23:35 +0500

    Need fixed

--- Main.py
+++ Main.py
@@ -4,9 +4,14 @@ import Visualization
 import Visual_2_0
 
 
-path = "experiments/dode.obj"
-size_voxel = 1
+path = "experiments/sphere.obj"
+size_voxel = 0.3
 model = ReadObj.read_file(path)
-voxels = MeshToVoxel.get_voxel_model(model[0], model[1], size_voxel)
-#Visualization.get_model(voxels, size_voxel)
-Visual_2_0.ShowModel(voxels, size_voxel, [[i[0] for i in model[1]], [i[1] for i in model[1]]], False)
+#не лениво
+voxels = [i for i in MeshToVoxel.get_voxel_model(model[0], model[1], size_voxel)]
+
+#лениво
+#voxels = MeshToVoxel.get_voxel_model(model[0], model[1], size_voxel)
+
+Visualization.get_model(voxels, size_voxel)
+Visual_2_0.ShowModel(voxels, size_voxel, [[i[0] for i in model[1]], [i[1] for i in model[1]]], True)
